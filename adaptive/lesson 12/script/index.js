1ead29ac95d3656156eed2efe7cdcbeeaa29a78c
bbbbatttyyyaaa <bekzanurustemov@gmail.com>
2022-11-09 11:59:14 +0600

    some done

---
+++ adaptive/lesson 12/script/index.js
@@ -0,0 +1,164 @@
+let userName = 'Alvida'
+let username = 'Alvida'
+
+console.log(username, userName )
+
+// === нельзя называть зарезервированным кодом - переменные
+// let let
+
+// let a = 25 
+// let b = 13
+// let c = a
+// c = b + a
+
+
+//  console.log(c);
+
+// let massage = 'Hello'
+// massage = 10
+
+// ===== String =====
+
+let str = 'string1'
+let str2 = "strinng2"
+let str3 = `string3`
+
+let num = 100500
+
+// console.log(str + '-' + num);
+// console.log(`Lorem - ${num}`);
+
+// console.log('lorem');
+
+
+// console.log('text 5 + 1');
+// console.log(`text ${5 + 1}`);
+// console.log('text' + 5 + 1);
+
+
+//===== Number =====
+// let num = 10
+// let num2 = 12.25
+// console.log(num + num2 )
+
+// let n = 5
+// let n2 = 0
+
+// console.log(-n / n2)
+// console.log(NaN); ==== Not A Number
+
+// console.log(n + '10'); 510
+// console.log(n * '10') 50
+// console.log(n / '10') 0.5
+// console.log(n - '10') -5
+
+// console.log( 10 / '10b') NaN
+// console.log(10 - '10s') NaN
+
+//boolean
+
+// let b1 = true  //Истина
+// let b2 = false // Ложь
+// console.log(b1,b2)
+
+// console.log( 5 = 5)
+// console.log(5 >= 5)
+// console.log(5 == '5')
+
+// Частица не! =========
+
+// console.log(!true)
+// console.log(!false)
+
+// let s = 5 > 2
+// console.log(s)
+
+// === Nul ========
+
+// let n = null
+// console.log(n)
+// // ====== underfined======
+// let age
+// age = underfined
+// console.log(age)
+
+// ======== typeof =======
+
+// console.log(type undefined);
+// console.log(tepeof 0)
+
+// console.log(typeof Symbol('id'))
+
+// ========== Преобразование типов данных ============
+
+// Преобразование строк
+
+// let value = true
+// // console.log(typeof value)
+// let value2 = String(value)
+// console.log(typeof value2)
+
+// console.log(String (true))
+
+//prompt - авто - преобразование в строку String
+
+
+// Преобразование чисел ==================
+
+// авто проебразование в Number 
+
+// Происходит склеивание (конкантинация)
+// console.log('10' + '5');
+
+
+// let str = '123'
+// console.log(typeof str)
+// let num = Number (str)
+// console.log(typeof num)
+
+// let n = Number('100')
+// let i = Number('500')
+
+// console.log(n + i) 
+
+// console.log(Number(undefined))
+// console.log(Number(null))
+
+// console.log(Number(true))
+
+// let str = 'hi'
+// console.log(Number(str))  // если это текст то будет NaN
+// console.log(Number('125'))
+// console.log(Number(''))
+// console.log(Number('124x')) NaN
+
+//============= Логические преобразования ==============
+
+// let b1 = Boolean(1)
+// console.log(b1) true
+
+
+// 0 , null , undefined ,NaN , '', (всо чо отрицательное) == false
+// Любые другие === true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
